6cc859dd9c9664d140d2bdf77fa2c3f35396adee
AFORANURAG <anuragupadhyay172912313@gmail.com>
2022-12-23 14:45:42 +0530

    passed by value and passed by reference explainer

---
+++ passedbyvspassedbyrefernce/index.js
@@ -0,0 +1,29 @@
+// let a =10
+// let b=a
+// b=b+1;
+// console.log(a,b)
+
+let c=["anurag","masai","128301"]
+let d=c
+d.push(12)
+let some=[]//8923048023
+some=[]//jklajkldjas
+console.log([1,2,3] ==[1,2,3] )
+//{}=={}-->false 
+//{1,2,3} {1,2,3} 
+///////////////////////////////////////////////////////////
+
+// console.log({}=={})
+// console.log([]==[])
+
+// let array=[1,2,3,4,5,6]
+
+// const fun=(array)=>{
+// array.push(12)
+// console.log(array)
+// array=[]
+// array.push(1213123)
+// console.log(array)
+// }
+
+// fun(array)
